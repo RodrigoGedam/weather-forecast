df1d2e4dd8f8a67acf2cf5add6148d3e3ffda971
Rodrigo Gedam <rogedam@gmail.com>
2023-07-12 18:58:10 -0300

    commit do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Forecast from './components/Forecast';
+import Search from './components/Search';
+import useForecast from './hooks/useForecast';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } = useForecast();
+
+	return (
+		<>
+			{forecast ? (
+				<main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-full 2xl:h-[100vh] w-full">
+					<Forecast data={forecast} />
+				</main>
+			) : (
+				<main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+					<Search
+						term={term}
+						options={options}
+						onInputChange={onInputChange}
+						onOptionSelect={onOptionSelect}
+						onSubmit={onSubmit}
+					/>
+				</main>
+			)}
+		</>
+	);
 }
 
 export default App;
---
+++ src/hooks/useForecast.ts
@@ -0,0 +1,68 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+import { ForecastT, OptionsT } from './../types';
+
+export default function useForecast() {
+	const [term, setTerm] = useState<string>('');
+	const [city, setCity] = useState<OptionsT | null>(null);
+	const [options, setOptions] = useState<[]>([]);
+	const [forecast, setForecast] = useState<ForecastT | null>(null);
+
+	const getSearchOption = (value: string) => {
+		fetch(
+			`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+				process.env.REACT_APP_API_KEY
+			}`
+		)
+			.then((res) => res.json())
+			.then((data) => setOptions(data))
+			.catch((e) => console.log(e));
+	};
+
+	const onOptionSelect = (option: OptionsT) => {
+		setCity(option);
+	};
+
+	const onSubmit = () => {
+		if (!city) return;
+
+		getForecast(city);
+	};
+
+	const getForecast = (city: OptionsT) => {
+		fetch(
+			`https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&units=metric&lang={pt_br}&appid=${process.env.REACT_APP_API_KEY}`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				const forecastData = {
+					...data.city,
+					list: data.list.slice(0, 16),
+				};
+				setForecast(forecastData);
+			})
+			.catch((e) => console.log(e));
+	};
+
+	const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value.trim();
+		setTerm(value);
+		if (value === '') return;
+		getSearchOption(value);
+	};
+
+	useEffect(() => {
+		if (city) {
+			setTerm(city.name);
+			setOptions([]);
+		}
+	}, [city]);
+
+	return {
+		term,
+		options,
+		forecast,
+		onInputChange,
+		onOptionSelect,
+		onSubmit,
+	};
+}
